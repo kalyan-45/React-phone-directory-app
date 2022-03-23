6054c0dcd814b3e9cd64b649fcdd35c01d14647c
Kalyan Shete <kalyanshete@01gmail.com>
2022-03-24 01:50:19 +0530

    Craeted the skeleton for the app

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles/Header.css';
+
+const Header = (props => {
+    return (
+        <header>
+            <span>{props.heading}</span>
+        </header>
+    )
+})
+
+export default Header;
