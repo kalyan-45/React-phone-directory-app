3c0a14fc0a91c5454e334071cb8e05faa78c5149
Kalyan Shete <kalyanshete@01gmail.com>
2022-03-31 00:59:56 +0530

    Created stateful the AddSubscriber page

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -4,7 +4,22 @@ import './Common/common.css';
 import './Styles/AddSubscriber.css'
 
 class AddSubscriber extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            id : 0,
+            name : '',
+            phone : ''
+        };
+    }
+
+    inputChangedHandler = (event) => {
+        const newState = this.state;
+        newState[event.target.name] = event.target.value;
+        this.setState(newState); 
+    } 
     render() {
+        const {name, phone} = this.state;
         return (
             <div className="component-body">
                 <Header heading = "Add Subscriber" />
@@ -13,14 +28,14 @@ class AddSubscriber extends React.Component {
 
                     <form action="" className="form-container">
                         <label htmlFor="name" className="form-label" >Name: </label>
-                        <input type="text" id="name" className="form-input" name="name" /> <br /><br />
+                        <input type="text" id="name" className="form-input" name="name" onChange={this.inputChangedHandler}/> <br /><br />
                         <label htmlFor="phone" className="form-label" >Phone: </label>
-                        <input type="text" id="phone" className="form-input" name="phone" /> <br /><br />
+                        <input type="text" id="phone" className="form-input" name="phone" onChange={this.inputChangedHandler} /> <br /><br />
 
                         <div className="info-container">
                             <span className="subscriber-add-heading">Subscriber To Be Added:</span><br />
-                            <span className="subscriber-info">Name: </span><br />
-                            <span className="subscriber-info">Phone: </span>
+                            <span className="subscriber-info">Name: {name} </span><br />
+                            <span className="subscriber-info">Phone: {phone} </span>
                         </div>
 
                         <button className="btn addBtn">Add</button>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import AddSubscriber from './AddSubscriber';
 
 ReactDOM.render(
@@ -9,4 +8,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
